48796fec498d8ccbbc16fa00d2bad87b4aa77e85
FAIZAH AHSAN <faizah1992@gmail.com>
2020-03-12 03:12:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,37 @@
-require 'pry'
+require "pry"
 
 class String
 
   def sentence?
-    
+   if self.end_with?(".")
+    return true
+   else 
+    return false
+   end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      false 
+    end
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else 
+      false 
+    end
 
   end
 
   def count_sentences
-
+   a= self.split.count
+   a.reduce 
+   
+   
   end
+   
 end
